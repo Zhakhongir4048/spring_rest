f85555156dcc11961345870d06baa363e86a14c9
Эсонов Жахонгир <dzhoha4048@mal.ru>
2023-06-19 00:39:01 +0300

    Best commit

--- src/main/java/com/example/spring_rest/exception_handling/UserIncorrectData.java
+++ src/main/java/com/example/spring_rest/exception_handling/UserIncorrectData.java
@@ -1,18 +1,14 @@
 package com.example.spring_rest.exception_handling;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserIncorrectData {
 
     private String info;
-
-    public UserIncorrectData() {
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
 
 }
